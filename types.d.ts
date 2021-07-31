a93195f4159cd7e341d3cfe1ddb641721626b3c9
Arnav Gosain <arnav@arnavgosain.com>
2021-07-31 19:55:13 +0530

    Fix types

--- types.d.ts
+++ types.d.ts
@@ -2,8 +2,6 @@ import { SerializeOptions } from "next-mdx-remote/dist/types";
 
 export type Options = SerializeOptions;
 
-declare module "next-mdx-fused" {
-  export default function nextMdxFused(
-    options: Options
-  ): (nextConfig: {}) => {};
+declare module "next-mdx-builder" {
+  export default (options: Options) => (nextConfig: {}) => {};
 }
